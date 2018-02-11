db1c33894c8632f67c4e5e8c7eed531aa39e0062
Yazgylych <y.rahymov@gmail.com>
2018-02-11 13:31:31 -0500

    Added friendship model

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: 'stocks#search'
+  get 'my_friends', to: "users#my_friends"
   devise_for :users, :controllers => { :registrations => "user/registrations" }
   resources :user_stocks, only: [:create, :destroy]
 end
